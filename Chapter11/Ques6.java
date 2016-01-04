dc19166c5833cde9c06ed25861083135da2ad230
wilkhu90 <sumeet.wilkhu@gmail.com>
2016-01-04 00:45:21 -0600

    Adding solution to problem 6.

--- Chapter11/Ques6.java
+++ Chapter11/Ques6.java
@@ -19,6 +19,6 @@ public class Ques6{
 		int[][] matrix = {{15,20,40,85},{20,35,80,95},
 			{30,55,95,105},
 			{40,80,100,120}};
-		System.out.println(solution.findflement(matrix, 55));
+		System.out.println(solution.findflement(matrix, 95));
 	}
 }
